3195553cb308c8293f4126cf1774920ef3917dc7
faizan-zahid-arhamsoft <faizan.zahid@arhamsoft.org>
2025-02-13 18:07:50 +0500

    .

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Body, Post } from '@nestjs/common';
 import { AuthService } from 'src/modules/auth/services';
-import { Body, Post, Request } from '@nestjs/common';
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
+
+  @Get('test')
+  async test() {
+    return 'test';
+  }
 
   @Post('send-notification')
   async sendNotification(@Body() notificationData: any) {
