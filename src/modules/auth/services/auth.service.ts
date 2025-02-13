149b217ccce9c18761ef3ce7636dbaac9b0e0de7
faizan-zahid-arhamsoft <faizan.zahid@arhamsoft.org>
2025-02-13 18:03:59 +0500

    ...

--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -8,17 +8,10 @@ export class AuthService {
   ) {}
 
   async sendNotification(notificationData: any): Promise<string> {
-    console.log('notificationData.title :', notificationData.title);
-    console.log('notificationData.body :', notificationData.body);
-    console.log('notificationData.fcmToken :', notificationData.fcmToken);
     this.notificationService.sendPushNotification(notificationData.fcmToken, {
       title: notificationData.title.toString(),
-      body: notificationData.body.toString(),   
-      data: {
-        title: notificationData.title.toString(),
-        body: notificationData.body.toString(),
-      }
-
+      body: notificationData.description.toString(),   
+      data: notificationData.body,
     });
     return 'yes'
   }
